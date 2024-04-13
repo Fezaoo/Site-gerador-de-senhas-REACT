d3d9b37da0398a3892769205dd710a8d9527d643
Felipe Guimarães <146668592+Fezaoo@users.noreply.github.com>
2024-04-13 18:11:36 -0300

    config option

--- src/App.js
+++ src/App.js
@@ -15,32 +15,60 @@ function App() {
           <div className='descricao'>
             <p>
               Utilize o nosso site online para criar uma senha forte e segura
-          </p>
+            </p>
           </div>
         </div>
       </header>
       <main className='corpo_container'>
         <section className='corpo'>
-          <div className='senha_section'>
+          <section className='senha_section'>
             <div className='senha_container'>
               <div className='senha'>
-                <p>pSdoaASDASDASDASDASDASDASDDASDASSDASDDASASDASDASDASDASDASDDds</p>
+                <p>pSdoaASDASDAASDASDASDASDASDASDDds</p>
               </div>
               <div className='senha_interacao'>
-              <button className='senha_interacao_botao'>
-                <MdOutlineContentCopy size={30} />
-              </button>
-              <button className='senha_interacao_botao'>
-                <GrPowerReset size={30}/>
-              </button>
+                <button className='senha_interacao_botao'>
+                  <MdOutlineContentCopy size={30} />
+                </button>
+                <button className='senha_interacao_botao'>
+                  <GrPowerReset size={30} />
+                </button>
               </div>
             </div>
-              <div className='status_bar_container'>
-                <div className='status_bar_externo status_bar'>
-                <div className='status_bar_interno status_bar'/>
+            <div className='status_bar_container'>
+              <div className='status_bar_externo status_bar'>
+                <div className='status_bar_interno status_bar' />
+              </div>
+            </div>
+          </section>
+          <section className='senha_config'>
+            <h2 className=''>
+              Personalizar
+            </h2>
+            <div className='config'>
+              <div className='senha_tamanho_container'>
+                <label className='label'>Tamanho: </label>
+                <input type='range' className='senha_tamanho' min={4} max={25} step={1}></input>
+              </div>
+              <div className='senha_caracteres_container'>
+                <label className='label'>Incluir</label>
+                <div className='checkbox_container'>
+                  <div className='checkbox_option'>
+                    <input type='checkbox'></input>
+                    <label className='label_checkbox'>Maiúsculas</label>
+                  </div>
+                  <div className='checkbox_option'>
+                    <input type='checkbox'></input>
+                    <label className='label_checkbox'>Minúsculas</label>
+                  </div>
+                  <div className='checkbox_option'>
+                    <input type='checkbox'></input>
+                    <label className='label_checkbox'>Símbolos</label>
+                  </div>
                 </div>
               </div>
-          </div>
+            </div>
+          </section>
         </section>
       </main>
     </div>
